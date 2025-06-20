3a12be29174a32cd25e4a011a7b51a1d78467a22
bukky <farinubukola1407@gmail.com>
2025-06-21 00:05:01 +0100

    Improvements made on the overall website

--- greeting.js
+++ greeting.js
@@ -3,3 +3,23 @@ const user = params.get("user"); //  null if ?user= is absent
 if (user) {
   document.getElementById("welcome").textContent = "Welcome, " + user + "!";
 }
+let bars = document.getElementById("bars");
+// bars.onclick = function () {
+//   let nav = document.getElementById("nav");
+//   if (nav.style.display === "block") {
+//     nav.style.display = "none";
+//   } else {
+//     nav.style.display = "block";
+//   }
+// }
+let nav = document.getElementById("nav");
+bars.addEventListener("click", function () {
+  const navVisible = nav.style.display === "block";
+
+  nav.style.display = navVisible ? "none" : "block";
+  bars.style.display = navVisible ? "block" : "none";
+})
+nav.addEventListener("mouseleave", function () {
+  nav.style.display = "none";
+    bars.style.display = "block";
+})
